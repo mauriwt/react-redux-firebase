b54f899d1c0cc8e8604ada7ac7738d055335f702
wtituana <wtituana@yachay.gob.ec>
2018-06-18 08:36:41 -0500

    Inicio de proyecto

---
+++ src/config/dev.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBCKvoWCg8rRKOmNQbsc_EZKueGXjycAKg",
+    authDomain: "react-callary.firebaseapp.com",
+    databaseURL: "https://react-callary.firebaseio.com/"
+  };
